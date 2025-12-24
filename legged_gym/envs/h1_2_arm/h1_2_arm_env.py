93bdac09fb32c774d215eef075676a1514344883
billy <1214592856@qq.com>
2025-12-24 21:36:07 +0800

    fix bug

--- legged_gym/envs/h1_2_arm/h1_2_arm_env.py
+++ legged_gym/envs/h1_2_arm/h1_2_arm_env.py
@@ -308,14 +308,14 @@ class H1_2ArmRobot(LeggedRobot):
                                     (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos, # 7
                                     self.dof_vel * self.obs_scales.dof_vel, # 7
                                     self.actions, # 7
-                                    self.contact_forces # 6
+                                    self.end_contact_forces # 6
                                     ),dim=-1)
         self.privileged_obs_buf = torch.cat((
                                     self.projected_gravity,
                                     (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
                                     self.dof_vel * self.obs_scales.dof_vel,
                                     self.actions,
-                                    self.contact_forces # 6
+                                    self.end_contact_forces # 6
                                     ),dim=-1)
         # add perceptive inputs if not blind
         # add noise if needed
